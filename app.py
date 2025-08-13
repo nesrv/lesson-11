315540a35fcfe32b233ce7433c23696396c88b16
nesrv <ne_srv@mail.ru>
2025-08-13 15:05:04 +0300

    начал работать с модулями

---
+++ app.py
@@ -0,0 +1,8 @@
+import my_module
+
+print ("hello world")
+
+
+x = my_module.summa(1.6, 2)
+
+print(x)
---
+++ my_module.py
@@ -0,0 +1,13 @@
+
+pi = 3.14
+
+def summa(a:int, b:float) -> int:
+    '''
+    эта функция 
+    делает a + b
+    '''
+    return a + b
+
+
+print("привет из модуля")
+
