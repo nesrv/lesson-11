40355ac2009fa3f672daab7f55f09a0f7389ca59
nesrv <ne_srv@mail.ru>
2025-08-15 14:57:50 +0300

    +

--- key_logger.py
+++ key_logger.py
@@ -1,15 +1,22 @@
 import keyboard
+import tkinter as tk
+from tkinter import scrolledtext
 
-
-def f(event):    
-    f = open("log.txt", "a")
-    f.write(event.name)
+def f():   
+    
+    root = tk.Tk()
+    root.title("Кейлогер")
+    root.geometry("400x300") 
+    
+    text_box = scrolledtext.ScrolledText(root, width=50, height=15)
+    text_box.pack(pady=10)
+    
+    root.mainloop()
         
 
 if __name__ == "__main__":
     print("Кейлогер запущен. Нажмите ESC для остановки.")
-    keyboard.on_press(f)
-    keyboard.wait("esc")
+    f()
+ 
     
   
-# fsdfdsf;lk;lk;
