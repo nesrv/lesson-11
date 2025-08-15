9af75bc91f1b6c07e233453b0055e2e967a496e1
nesrv <ne_srv@mail.ru>
2025-08-15 14:17:25 +0300

    изучение радиокнопок

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 import tkinter as tk
 from my_module import study_entry
+from study_radiobutton import study_radio
 
 
 if __name__ == '__main__':
     root = tk.Tk()
     root.title("Изучение элементов GUI")
     root.geometry("400x300")
-    tk.Button(root, text="Изучение полей ввода", command=study_entry).pack(pady=20)
+    
+    tk.Button(root,
+              text="Изучение полей ввода", 
+              command=study_entry).pack(pady=20)
+    
+    tk.Button(root,
+              text="Изучение радиокнопок", 
+              command=study_radio).pack(pady=20)
     
     
     root.mainloop()
