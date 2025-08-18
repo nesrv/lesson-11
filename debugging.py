c25baadc5f889cafa3345780fb4fcbf3fed03def
nesrv <ne_srv@mail.ru>
2025-08-18 16:20:56 +0300

    +

--- debugging.py
+++ debugging.py
@@ -1,8 +1,6 @@
 import logging
 import colorlog
 
-logging.basicConfig(level=logging.DEBUG)
-
 # Настройка цветного логирования
 handler = colorlog.StreamHandler()
 handler.setFormatter(colorlog.ColoredFormatter(
@@ -11,9 +9,19 @@ handler.setFormatter(colorlog.ColoredFormatter(
 
 logger = colorlog.getLogger('example')
 logger.addHandler(handler)
+logger.setLevel(logging.ERROR)
+logger.propagate = False  # Отключаем передачу в родительский логгер
 
-logger.debug("Подробная информация для отладки")      # DEBUG
-logger.info("Общая информация о работе")
-logger.warning("Предупреждение о проблеме")   
-logger.error("Ошибка в программе")                    # ERROR
-logger.critical("Критическая ошибка!")      
+
+# Теперь логи будут цветными
+logger.debug("Подробная информация для отладки")      # СЕРЫЙ
+logger.info("Общая информация о работе")              # БЕЛЫЙ  
+logger.warning("Предупреждение о проблеме")           # ЖЕЛТЫЙ
+logger.error("Ошибка в программе")                    # КРАСНЫЙ
+
+# если не удалось подключиться к базе данных
+try:
+    # код подключения к базе данных
+    pass
+except:
+    logger.critical("Критическая ошибка!")     
